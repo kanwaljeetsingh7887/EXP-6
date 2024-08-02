063f6b455e8c36a6ce9e108f3aca6cc5331a94c7
Kanwaljeet Singh <97405821+kanwaljeetsingh7887@users.noreply.github.com>
2024-08-02 09:55:25 +0530

    Add files via upload

---
+++ EXP-6B.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    cout<<"Enter a number: "<<"\n";
+    cin>>i;
+    while(i<=10)
+    {
+        cout<<" "<<"\n"<<i++;
+    
+    }
+    return 0;
+}5
